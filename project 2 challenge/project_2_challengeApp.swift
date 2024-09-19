484fa4b8c65ed919582d65ee0e4045f66ffab0d8
Evan <evan.sherfey@stu.barren.kyschools.us>
2024-09-19 08:08:01 -0500

    Initial Commit

---
+++ project 2 challenge/project_2_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_2_challengeApp.swift
+//  project 2 challenge
+//
+//  Created by Student on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct project_2_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
